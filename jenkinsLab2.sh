78bbf917226f8f1e708d3c5368688ad08bd4b9e7
neilvbell <neilvbell@yahoo.co.uk>
2025-07-17 15:20:21 +0100

    Update jenkinsLab2.sh

--- jenkinsLab2.sh
+++ jenkinsLab2.sh
@@ -1,5 +1,2 @@
-			ls -a ~
-			touch jenkinsGit.sh
-			echo "echo 'GitHub says Hi!'" > jenkinsGit.sh
-			chmod +x jenkinsGit.sh
-			./jenkinsGit.sh
+ls -a ~
+echo "echo 'GitHub says Hi!'" > jenkinsGit.sh
