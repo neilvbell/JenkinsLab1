9b8b978ee10504425eedf8510495f8959b1385c5
neilvbell <neilvbell@yahoo.co.uk>
2025-07-17 15:03:17 +0100

    Update jenkinsLab2.sh

--- jenkinsLab2.sh
+++ jenkinsLab2.sh
@@ -1,5 +1,5 @@
 			ls -a ~
-			touch coolScript.sh
-			echo "echo 'Hello World!'" > coolScript.sh
-			chmod +x coolScript.sh
-			./coolScript.sh
+			touch jenkinsGit.sh
+			echo "echo 'GitHub says Hi!'" > jenkinsGit.sh
+			chmod +x jenkinsGit.sh
+			./jenkinsGit.sh
